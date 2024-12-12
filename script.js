c816036c49de811b9e33db6c4555a84b5e4ef749
Piptr3 <piptr24@gmail.com>
2024-12-12 21:03:57 +0100

    add button

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const container = document.querySelector(".board");
-
-    for(let i = 0; i < 16; i++ ) {
-        const column = document.createElement("div");
+container.style.border = "solid black 2px"
+for(let i = 0; i < 16; i++ ) {
+    const column = document.createElement("div");
 
     for(let j = 0; j < 16; j++) {
         const row = document.createElement("div");
-        row.style.border = "red solid 1px";
-        row.style.width = "20px";
-        row.style.height = "20px";
-        column.appendChild(row)
+        row.style.width = "30px";
+        row.style.height = "30px";
+        row.classList.add("square");
+        column.appendChild(row);
     }
+    
     container.appendChild(column);
     }
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = 'Black';
+    })
+})
+
+const clearButton = document.querySelector(".btn");
+
+clearButton.addEventListener('click', () => {
+    const squares = document.querySelectorAll(".square")
+    squares.forEach((square) => {
+        square.style.backgroundColor = 'white';
+    })
+})
