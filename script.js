bc5e06f196b9fcd6d41c7bd43938312d783744c1
Piptr3 <piptr24@gmail.com>
2024-12-12 20:22:05 +0100

    create a grid

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-const container = document.querySelector(".board")
-container.style.backgroundColor = 'green';
+const container = document.querySelector(".board");
+
+    for(let i = 0; i < 16; i++ ) {
+        const column = document.createElement("div");
+
+    for(let j = 0; j < 16; j++) {
+        const row = document.createElement("div");
+        row.style.border = "red solid 1px";
+        row.style.width = "20px";
+        row.style.height = "20px";
+        column.appendChild(row)
+    }
+    container.appendChild(column);
+    }
